31f9d522432e8f0ff8a5797fecec5f19a7b32a06
Welcome Molefe <168111139+Melcomex@users.noreply.github.com>
2025-06-08 16:20:45 +0200

    Update student.h

--- student.h
+++ student.h
@@ -1,1 +1,23 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+
+class Student {
+private:
+    std::string name;
+    int id;
+    float marks[3]; // marks for 3 subjects
+    float average;
+    char grade;
+
+public:
+    void input();
+    void calculate();
+    void display() const;
+    void saveToFile(std::ofstream &out) const;
+    void loadFromFile(std::ifstream &in);
+};
+
+#endif
 
